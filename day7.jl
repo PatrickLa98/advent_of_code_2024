47c9ef713f2ce02e1879c9c4b7dc97b3cb360833
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-12-09 11:54:26 +0100

    added day7 partB

--- day7.jl
+++ day7.jl
@@ -19,7 +19,7 @@ end
 # PART A
 
 ## recursive function to find a correct solution of equation
-function find_correct_solution(values, solution, focal_index = 1, focal_value = focal_values[1])
+function find_correct_solution(focal_values, solution, focal_index = 1, focal_value = focal_values[1])
 
     ## if done iterating check if solution is correct
     if focal_index == length(focal_values)
@@ -44,7 +44,7 @@ for j in 1:length(solutions)
 
     focal_values = values[j]
     solution = solutions[j]
-
+      
    if find_correct_solution(focal_values, solution)
 
         result += solutions[j]
@@ -55,3 +55,34 @@ result
 
 
 # PART B
+
+
+## recursive function to find a correct solution of equation with added concatenation option
+function find_correct_solution(focal_values, solution, focal_index = 1, focal_value = focal_values[1], can_concatenate = true) ## initially set to true
+
+    ## if done iterating check if solution is correct
+    if focal_index == length(focal_values)
+
+        return focal_value == solution
+    end
+
+    ## update iteration
+    next_index = focal_index + 1
+   
+    ## when not concatenated yet give 3 options and update can_concatenate if used
+    if can_concatenate
+
+        can_concatenate = false
+
+        return find_correct_solution(focal_values, solution, next_index, focal_value + focal_values[next_index]) ||
+               find_correct_solution(focal_values, solution, next_index, focal_value * focal_values[next_index]) ||
+               find_correct_solution(focal_values, solution, next_index, parse(Int, string(focal_value)*string(focal_values[next_index])))
+
+    else
+
+        ## Recursion with alternative + or * operator
+        return find_correct_solution(focal_values, solution, next_index, focal_value + focal_values[next_index]) ||
+            find_correct_solution(focal_values, solution, next_index, focal_value * focal_values[next_index])
+
+    end
+end

d73f850ac3a41e4858d7423d83edc9ebb4d8261d
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-12-17 15:48:07 +0100

    added day3 partB

--- day3.jl
+++ day3.jl
@@ -22,3 +22,40 @@ result
 
 
 # PART B
+
+correct_pattern =  r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))"
+
+
+## identify correct patterns in data
+matches = eachmatch(correct_pattern, d)
+
+matches = collect(matches)
+
+## initialize result object
+result = 0
+
+## initiate do as baseline true
+do_multiply = true
+
+
+## loop throgh matches and add up multiplied matches if do_multiply is true
+
+for m in matches
+
+    if m.match == "do()"
+
+        do_multiply = true
+
+    elseif m.match == "don't()"
+
+        do_multiply = false
+
+    elseif do_multiply 
+
+        x = parse(Int, m.captures[2])  # convert weird string object to integer
+        y = parse(Int, m.captures[3])  # convert weird string object to integer
+        result  += x * y # multiply and add to results
+    end
+end
+
+result
